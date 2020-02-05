744ec558c2422b3c96daaa7f1b7f90a144bed586
larryfraz <larryfraz@yahoo.com>
2020-02-05 10:34:24 -0500

    Create uart.h

---
+++ .github/workflows/uart.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Hardware UART example for MSP430.
+ *
+ * Stefan Wendler
+ * sw@kaltpost.de
+ * http://gpio.kaltpost.de
+ *
+ *****************************************************************************/
+
+#ifndef __UART_H
+#define __UART_H
+
+/**
+ * Initialize soft UART
+ */
+void uart_init(void);
+
+/**
+ * Set pointer for ISR to call when data was received.
+ *
+ * @param[in]	*isr_ptr	pointer to ISR
+ */
+void uart_set_rx_isr_ptr(void (*isr_ptr)(unsigned char c));
+
+/**
+ * Read one character from UART blocking.
+ *
+ * @return	character received 
+ */
+unsigned char uart_getc();
+
+/**return next byte in buffer
+ * * @return next byte recieved
+ *
+ */
+unsigned char uart_getByte();
+/**
+ * Write one chracter to the UART blocking.
+ *
+ * @param[in]	*c	the character to write
+ */
+void uart_putc(unsigned char c);
+
+/**
+ * Write string to the UART blocking.
+ *
+ * @param[in]	*str	the 0 terminated string to write
+ */
+void uart_puts(const char *str);
+
+#endif
+
